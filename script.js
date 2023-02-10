6fd75184b64d1a1e9b7187ad4a582cdded4d4b65
Maria <Maria@Users-MacBook-Pro.local>
2023-02-10 14:17:16 +0200

    add js todo functions

--- script.js
+++ script.js
@@ -1,8 +1,62 @@
-const taskInput = document.querySelector('.task-input input');
+const taskInput = document.querySelector(".task-input input");
+taskBox = document.querySelector(".task-box")
 
-taskInput.addEventListener('keyup', e=>{
-    let userTask = taskInput.value;
-    if(e.key ==='Enter'){
-        console.log(userTask)
+let todos = JSON.parse(localStorage.getItem("todo-list"));
+
+function showTodo() {
+  let li = "";
+  if(todos){
+    todos.forEach((todo, id) => {
+        let isCompleted = todo.status === "completed" ? "checked" : "";
+        li += `<li class="task">
+                <label for="${id}">
+                    <input onclick="updateStatus(this)" type="checkbox" id="${id}" 
+                    ${isCompleted} />
+                    <p class = "${isCompleted}">${todo.name}</p>
+                </label>
+                <div class="settings">
+                <i class="uil uil-ellipsis-h"></i>
+                <ul class="task-menu">
+                    <li><i class="uil uil-pen"></i>Edit</li>
+                    <li><i class="uil uil-trash"></i>Delete</li>
+                </ul>
+            </div>
+        </li>
+    `;
+      });
+  }
+  
+  taskBox.innerHTML = li;
+}
+showTodo();
+
+function updateStatus(selectedTask){
+let taskName = selectedTask.parentElement.lastElementChild;
+if(selectedTask.checked){
+    taskName.classList.add("checked");
+    todos[selectedTask.id].status = "completed"
+}else{
+    taskName.classList.remove("checked");
+    todos[selectedTask.id].status = "pending"
+
+}
+localStorage.setItem("todo-list", JSON.stringify(todos));
+
+}
+
+
+
+
+taskInput.addEventListener("keyup", (e) => {
+  let userTask = taskInput.value.trim();
+  if (e.key === "Enter" && userTask) {
+    if (!todos) {
+      todos = [];
     }
-})
+    taskInput.value = "";
+    let taskInfo = { name: userTask, status: "pending" };
+    todos.push(taskInfo);
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo();
+  }
+});
